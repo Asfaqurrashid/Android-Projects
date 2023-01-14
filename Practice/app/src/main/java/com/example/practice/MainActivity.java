a1d20a028ce8c5a6cabe8dfc2155f9ce412c59f7
asfaqur <asfaqurrashid@gmail.com>
2023-01-14 18:23:21 +0600

    initial commit

---
+++ Practice/app/src/main/java/com/example/practice/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
